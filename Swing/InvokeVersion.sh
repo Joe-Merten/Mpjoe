e7b7e31a9c6999837c5a7f456687ecf40c3735eb
Joe Merten <git@jme.de>
2015-01-12 21:16:03 +0100

    Git Rev in Versionsinfo eingebunden

--- Swing/InvokeVersion.sh
+++ Swing/InvokeVersion.sh
@@ -9,36 +9,30 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # Dieses Skript ist in das Maven-Skript "pom.xml" eingebunden und versorgt dieses bei jedem Build mit dem aktuellen
 # Versionsstring.
-# Der generierte Versionsstring hat dann die Form: "Mpjoe Java Client V1 2013-08-12 16:33:37 svn-r201M"
+# Der generierte Versionsstring hat dann die Form: "Mpjoe Java Client V1 2013-08-12 16:33:37, git rev 21:808a..."
 #-----------------------------------------------------------------------------------------------------------------------
 # Siehe auch: http://stackoverflow.com/questions/690419/build-and-version-numbering-for-java-projects-ant-cvs-hudson
 ########################################################################################################################
 
-declare SVN_REV=""
-declare SVN_MOD=""
-declare SVN_INFO=""
-
 declare VER_TITLE="Mpjoe Java Client"
 declare VER_NUMBER="1"
 declare VER_DATETIME=""
-declare VER_SVN=""
+declare VER_SOURCE=""
 declare VER_STRING=""
 declare VER_DEVELOPER=""
 
 # Ermilltung der Versionsinformationen zur Kompilierzeit (packen des jar Archivs)
 if [ "$1" == "--make-version-string" ]; then
-    # Svn Informationen einholen. Bei Fehler (z.B. kein Svn), dann Leerstring
-    #VER_SVN="$(svnversion -n)"
-    #[ "$VER_SVN" != "" ] && VER_SVN="svn-r$VER_SVN"
-    VER_SVN="TODO"
+    # Git Informationen einholen.
+    VER_SOURCE=", git rev $(../Common/make/gitversion)"
 
     # Build Datum "YYYY-MM-DD" und Uhrzeit "HH:MM:SS" (UTC)
-    VER_DATETIME="`date -u +"%Y-%m-%d %H:%M:%S"`"
+    VER_DATETIME="$(date -u +"%Y-%m-%d %H:%M:%S")"
 
     # Sowas wie VER_DEVELOPER ist (noch) nicht implementiert
 
     # SoftwareVersionString zusammensetzen
-    VER_STRING="$VER_TITLE V$VER_NUMBER $VER_DATETIME $VER_SVN$VER_DEVELOPER"
+    VER_STRING="$VER_TITLE V$VER_NUMBER $VER_DATETIME $VER_SOURCE$VER_DEVELOPER"
 fi
 
 if [ "$VER_STRING" != "" ]; then
