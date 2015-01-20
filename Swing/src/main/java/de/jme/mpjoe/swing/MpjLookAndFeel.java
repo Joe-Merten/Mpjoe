aae49670dbdf278b12b94f089f9c7c26dd4d3b16
Joe Merten <git@jme.de>
2015-01-21 00:23:02 +0100

    Aqua DarkTheme erst mal wieder abgeschaltet

--- Swing/src/main/java/de/jme/mpjoe/swing/MpjLookAndFeel.java
+++ Swing/src/main/java/de/jme/mpjoe/swing/MpjLookAndFeel.java
@@ -61,7 +61,7 @@ public class MpjLookAndFeel {
                 setNimbusDarkTheme();
             } else {
                 // Apple
-                setAquaDarkTheme();
+                //setAquaDarkTheme();
             }
 
             // printLookAndFeels();
