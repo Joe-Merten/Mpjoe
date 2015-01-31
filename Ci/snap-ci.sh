8821ac517df4c82f299d34840fa4918b6d5e9129
Joe Merten <git@jme.de>
2015-01-31 12:36:01 +0100

    Buildskript: Noch mehr Debugoutput

--- Ci/snap-ci.sh
+++ Ci/snap-ci.sh
@@ -7,28 +7,30 @@
 # \creation   2015-01-30, Joe Merten
 #-----------------------------------------------------------------------------------------------------------------------
 # Dieses Buildskript wird via snap-ci.com angeworfen
-#
-# snap-ci am 31.01.2015:
-# - java 1.8.0_31
-# - maven 3.2.3
-# -
 ########################################################################################################################
 
+# snap-ci am 31.01.2015:
+# - Linux ct-10-0-130-14 2.6.32-042stab102.9 #1 SMP Fri Dec 19 20:34:40 MSK 2014 x86_64 x86_64 x86_64 GNU/Linux
+# - java 1.8.0_31
+# - maven 3.2.3
 echo "===== linux version ====="
 uname -a
 echo "===== maven version ====="
 mvn --version
 echo "========================="
 
-echo "===== davor ====="
-
+# Android Sdk wird installiert nach $ANDROID_HOME="/var/go/android"
 ./snap-setup-android.sh
 
-echo "===== dazwischen ====="
-
+echo "=== android sdk ========="
 ls -l /var/go/android
-
-echo "===== danach ====="
+echo "=== android sdk build-tools ========="
+ls -l /var/go/android/build-tools
+echo "=== android sdk platforms ========="
+ls -l /var/go/android/platforms
+echo "=== android platform-tools ========"
+ls -l /var/go/android/platform-tools
+echo "========================="
 
 ./snap-build.sh
 ./snap-test.sh
